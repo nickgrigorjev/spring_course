9cb3f521f3c8c64ff5e47d8147793dd80718e79c
Nick Grigorjev <ns.grigorjev@gmail.com>
2023-02-13 14:57:49 +0400

    027 Выражение Pointcut. Часть 2

---
+++ src/main/java/aop/Test1.java
@@ -0,0 +1,21 @@
+package aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test1 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Book book = context.getBean("book", Book.class);
+
+        UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
+        uniLibrary.getBook(book);
+//        uniLibrary.returnBook();
+//        uniLibrary.getMagazine();
+
+//        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+//        schoolLibrary.getBook();
+
+        context.close();
+    }
+}
---
+++ src/main/java/aop/UniLibrary.java
@@ -0,0 +1,26 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniLibrary extends AbstractLibrary {
+
+    public void getBook(Book book) {
+        System.out.println("Мы берем книгу из UniLibrary " + book.getName());
+    }
+
+
+//    public void returnBook() {
+//        System.out.println("Мы возвращаем книгу в UniLibrary");
+//    }
+
+    protected String returnBook() {
+        System.out.println("Мы возвращаем книгу в UniLibrary");
+        return "OK";
+    }
+
+
+    public void getMagazine() {
+        System.out.println("Мы берем журнал из UniLibrary");
+    }
+}
